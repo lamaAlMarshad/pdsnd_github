9dd56fd185431e670b163967d7170b77ebef071f
Lama AlMarshad <53469929+lamaAlMarshad@users.noreply.github.com>
2021-03-26 20:37:20 +0300

    add comments
     Added a comments line in 4 describe cit-date var what is it

--- bikeshare (1).py
+++ bikeshare (1).py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-
+# this var is type of dictionar the key is the na,e of data files and the vaule is the data files
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
